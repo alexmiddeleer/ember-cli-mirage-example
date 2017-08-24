5a70faac1f97278c2cbb92974f92358b817c4c8f
Alex <almiddeleer@gmail.com>
2017-08-24 14:50:59 -0400

    add a serializer

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -1,4 +1,12 @@
 import { JSONAPISerializer } from 'ember-cli-mirage';
 
 export default JSONAPISerializer.extend({
+  serialize(object, request) {
+    // This is how to call super, as Mirage borrows [Backbone's implementation of extend](http://backbonejs.org/#Model-extend)
+    let json = JSONAPISerializer.prototype.serialize.apply(this, arguments);
+
+    // Add metadata, sort parts of the response, etc.
+
+    return json;
+  }
 });
